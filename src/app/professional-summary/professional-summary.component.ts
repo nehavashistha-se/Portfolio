a906ef825a69a02c6d1fe75cc210c8bceeed4a6e
nehavashistha05 <nehavashistha05@gmail.com>
2024-02-04 00:10:35 +0530

    new changes

---
+++ src/app/professional-summary/professional-summary.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-professional-summary',
+  standalone: true,
+  imports: [],
+  templateUrl: './professional-summary.component.html',
+  styleUrl: './professional-summary.component.less'
+})
+export class ProfessionalSummaryComponent {
+
+}
---
+++ src/app/softskill/softskill.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-softskill',
+  standalone: true,
+  imports: [],
+  templateUrl: './softskill.component.html',
+  styleUrl: './softskill.component.less'
+})
+export class SoftskillComponent {
+
+}
