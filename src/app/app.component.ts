ac561520c7bbd377aab4734abd279e8f8bfea700
nehavashistha05 <nehavashistha05@gmail.com>
2024-02-04 00:09:37 +0530

    new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { TechnicalskillComponent } from "./technicalskill/technicalskill.component";
+import { SoftskillComponent } from "./softskill/softskill.component";
+import { ProfessionalSummaryComponent } from "./professional-summary/professional-summary.component";
+import { ExperienceComponent } from "./experience/experience.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.less',
+    imports: [RouterOutlet, HomeComponent, TechnicalskillComponent, SoftskillComponent, ProfessionalSummaryComponent, ExperienceComponent]
 })
 export class AppComponent {
   title = 'portfolio';
